a997d7d62e9e9a7411aa7f3b56bd9e8fadac8189
David Edwards <david.edwards@colostate.edu>
2017-04-19 07:15:06 -0600

    strip out first line

--- load_flight_data_into_hdfs.sh
+++ load_flight_data_into_hdfs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1987..2008}
 do
-    curl http://stat-computing.org/dataexpo/2009/$i.csv.bz2 | bzip2 -d | hdfs dfs -copyFromLocal - /data/$i.csv
+    curl http://stat-computing.org/dataexpo/2009/$i.csv.bz2 | bzip2 -d | sed -e 1d | hdfs dfs -copyFromLocal - /data/$i.csv
 done
