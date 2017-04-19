2bc6c021af2d1a3e0a560eb29d2224185807d255
bdeining <benjamin.deininger@connexta.com>
2017-04-18 20:48:49 -0600

    Added script to get flight data and parse properly

---
+++ get_flight_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in {1987..2008}
+do
+#   wget "http://stat-computing.org/dataexpo/2009/$i.csv.bz2"
+   bzip2 -d "$i.csv.bz2"
+   tail -n +2 "$i.csv" > "$i.csv.tmp" && mv "$i.csv.tmp" "$i.csv"
+done
